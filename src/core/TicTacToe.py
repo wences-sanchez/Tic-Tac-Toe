ed11a504af581b5df768b2a5cdaaf85607055b9c
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-07-23 11:50:10 +0200

    Add __str__() to TicTacToe class
    Implemented custom version of str()

--- src/core/TicTacToe.py
+++ src/core/TicTacToe.py
@@ -1,11 +1,25 @@
 
 
 class TicTacToe:
+    BOARD_SIZE = 3
+
     def __init__(self):
-        self.table = [['X', 'O', 'X'],
+        self.board = [['X', 'O', 'X'],
                       ['O', 'O', 'X'],
                       ['X', 'X', 'O']]
 
-    def get_table(self):
-        return self.table
+    def get_board(self):
+        return self.board
 
+    def __str__(self):
+        out = ''
+        for row in range(TicTacToe.BOARD_SIZE):
+            for col in range(TicTacToe.BOARD_SIZE):
+                out += self.board[row][col] + ' ' if col < TicTacToe.BOARD_SIZE - 1 else self.board[row][col]
+            out += '\n' if row < TicTacToe.BOARD_SIZE - 1 else ''
+        return out
+
+
+if __name__ == '__main__':
+    tic_tac_toe = TicTacToe()
+    print(tic_tac_toe)
