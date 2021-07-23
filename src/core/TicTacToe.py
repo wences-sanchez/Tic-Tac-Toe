76a48fad0504a052fb9ea786848916e9e6af30e6
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-07-23 09:51:00 +0200

    Implementation of the first successful test.

---
+++ src/core/TicTacToe.py
@@ -0,0 +1,11 @@
+
+
+class TicTacToe:
+    def __init__(self):
+        self.table = [['X', 'O', 'X'],
+                      ['O', 'O', 'X'],
+                      ['X', 'X', 'O']]
+
+    def get_table(self):
+        return self.table
+
