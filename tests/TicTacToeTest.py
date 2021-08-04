c8b73c7c641f447ee3459b1f0c2defa3a1d7b902
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-08-04 18:01:50 +0200

    Add test cases to the test suite
    Added:
    * test case of final winner ('X' or 'O')
    * test case of whether draw or not
    * test case of whether impossible board or not (reachable in game)

--- tests/TicTacToeTest.py
+++ tests/TicTacToeTest.py
@@ -1,5 +1,6 @@
 import unittest
 
+from parameterized import parameterized
 from src.core.TicTacToe import TicTacToe
 
 
@@ -24,6 +25,30 @@ class TicTacToeTest(unittest.TestCase):
         tic_tac_toe = TicTacToe('_X_O__OXX')
         expected_board = [['_', 'X', '_'], ['O', '_', '_'], ['O', 'X', 'X']]
         self.assertListEqual(expected_board, tic_tac_toe.get_board())
+
+    @parameterized.expand([
+        ('XXXO_O__O', 'X', True),
+        ('O_O__OXXX', 'X', True),
+        ('XOX_O__OX', 'O', True),
+        ('XOX_XO_OX', 'X', True),
+        ('_XO_OXO_X', 'O', True),
+        ('XXXO_O__O', 'O', False),
+        ('O_O__OXXX', 'O', False),
+        ('XOX_O__OX', 'X', False),
+        ('XOX_XO_OX', 'O', False),
+        ('_XO_OXO_X', 'X', False),
+    ])
+    def test_player_won(self, board, player, expected):
+        tic_tac_toe = TicTacToe(board)
+        self.assertEqual(expected, tic_tac_toe.has_won(player))
+
+    def test_game_is_draw(self):
+        tic_tac_toe = TicTacToe('XXOOXXXOO')
+        self.assertTrue(tic_tac_toe.is_draw())
+
+    def test_game_is_impossible(self):
+        tic_tac_toe = TicTacToe('XXXOOOXOX')
+        self.assertTrue(tic_tac_toe.is_impossible())
 
 
 if __name__ == '__main__':
